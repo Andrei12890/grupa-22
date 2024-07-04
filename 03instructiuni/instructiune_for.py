d419371d6a1ef89ccf5c0c06ec90a001369db20e
Andrei Gheorghita <andrey_890@yahoo.com>
2024-07-04 20:30:17 +0200

    instructiuni

---
+++ 03instructiuni/instructiune_for.py
@@ -0,0 +1,13 @@
+lista = [1, 2, 3]
+# for i in lista:
+#     print(i)
+# for i, v in enumerate(lista):
+#     print(i, v)
+# for i in "Ana are mere":
+#     print(i)
+for andrei in "Ana are mere":
+    print(andrei, 'temporar')
+print(i)
+
+lista_cumparaturi = ["struguri", "mere", "pere"]
+for index, value in enumerate
