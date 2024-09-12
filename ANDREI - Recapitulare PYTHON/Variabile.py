9073438d1519da1222efcdc24388049ec20f2887
Andrei Gheorghita <andrey_890@yahoo.com>
2024-09-12 20:32:34 +0200

    ETLEX

---
+++ ANDREI - Recapitulare PYTHON/Variabile.py
@@ -0,0 +1,18 @@
+# my_var = 5
+# my_var += 3
+# print(my_var)
+# my_var = my_var + 3
+# print(my_var)
+# my_var *= 2
+# print(my_var)
+# my_var = 8
+# my_var *= 2
+# print(my_var)
+# my_var = my_var * 2
+# print(my_var)
+my_var = 16
+# my_var >>= 4
+my_var = my_var >> 4
+print(my_var)
+my_var <<= 3
+print(my_var)
